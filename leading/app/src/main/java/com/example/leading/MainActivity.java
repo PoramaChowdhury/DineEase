56cddf2c13df1c87bb5deb7a7851f353cb8cb24a
pcpri <cse_182210012101023@lus.ac.bd>
2024-06-29 02:00:29 +0600

    Login

--- leading/app/src/main/java/com/example/leading/MainActivity.java
+++ leading/app/src/main/java/com/example/leading/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.leading;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +21,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+
+        ImageView photo;
+        TextView welcome,wish,forget_pass;
+        EditText user,password;
+        Button button_login,button_signup;
+
+        photo =  findViewById(R.id.photo);
+        user = findViewById(R.id.user_name);
+        password = findViewById(R.id.password);
+        button_login = findViewById(R.id.button_logIn);
+        button_signup = findViewById(R.id.button_signUp);
+
+       /* button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              String userName = user.getText().toString();
+              String Password = password.getText().toString();
+            }
+        });*/
+
+
+
+        button_signup.setOnClickListener(new View.OnClickListener(){
+         @Override
+         public void onClick(View v) {
+           Intent register;
+
+             register = new Intent(MainActivity.this,Register.class);
+             startActivity(register);
+         }
         });
+
+
+
+
+
     }
 }
